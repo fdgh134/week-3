e9d98cf24bf01b64f874e4d5604de6300175d39d
yoom <fdgh134@naver.com>
2024-12-03 21:40:42 +0900

    react 2week

--- week-2/src/App.js
+++ week-2/src/App.js
@@ -1,6 +1,6 @@
 //import styled from "styled-components";
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 // const StBox = styled.div`
 //   width: 100px;
@@ -45,22 +45,95 @@ import { useState } from 'react';
 //   );
 // }
 
+// function App() {
+//   const [number, setNumber] = useState(0);
+//   return (
+//     <div>
+//       <div>{number}</div>
+//       <button
+//         onClick = {() => {
+//           setNumber((beforeState) => beforeState + 1);
+//           setNumber((beforeState) => beforeState + 1);
+//           setNumber((beforeState) => beforeState + 1);
+//         }}
+//       >
+//         버튼
+//       </button>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const ref = useRef("초기값");
+//   console.log("ref 1", ref);
+//   ref.current = "바꾼 값";
+//   console.log("ref 1", ref);
+
+//   return (
+//     <div>
+//       <p>useRef</p>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const countRef = useRef(0);
+
+//   const plusStateCountButtonHandler = () => {
+//     setCount(count + 1);
+//   };
+
+//   const plusRefCountButtonHandler = () => {
+//     countRef.current++;
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         state 영역입니다. {count} <br />
+//         <button onClick={plusStateCountButtonHandler}>state 증가</button>
+//       </div>
+//       <div>
+//         ref 영역입니다. {countRef.current} <br />
+//         <button onClick={plusRefCountButtonHandler}>ref 증가</button>
+//       </div>
+//     </>
+//   );
+// }
+
 function App() {
-  const [number, setNumber] = useState(0);
+  const idRef = useRef("");
+  const pwRef = useRef("");
+  
+  const [id, setId] = useState("");
+  
+  const changeTap = (e) => {
+  	setId(e.target.value);
+  };
+  
+  // 랜더링 될 때
+  useEffect(() => {
+  	idRef.current.focus();
+  },[]);
+  
+  useEffect(() => {
+  	if ( id.length >= 10 ) {
+      pwRef.current.focus();
+    }
+  },[id]);
+  
   return (
-    <div>
-      <div>{number}</div>
-      <button
-        onClick = {() => {
-          setNumber((beforeState) => beforeState + 1);
-          setNumber((beforeState) => beforeState + 1);
-          setNumber((beforeState) => beforeState + 1);
-        }}
-      >
-        버튼
-      </button>
-    </div>
+    <>
+      <div>
+        아이디 : <input type="text" ref={idRef} value={id} onChange={changeTap}/>
+      </div>
+      <div>
+        비밀번호 : <input type="password" ref={pwRef} />
+      </div>
+    </>
   );
 }
+
 
 export default App;
