17f6731071ef3506bce1025de8e6668049d9e588
yoom <fdgh134@naver.com>
2024-12-04 22:00:44 +0900

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,67 @@
 //   return <GrandFather />
 // }
 
-import React, { useCallback, useState } from "react";
-import Box1 from "./components/Box1";
-import Box2 from "./components/Box2";
-import Box3 from "./components/Box3";
+// import React, { useCallback, useState } from "react";
+// import Box1 from "./components/Box1";
+// import Box2 from "./components/Box2";
+// import Box3 from "./components/Box3";
 
-const boxesStyle = {
-  display: "flex",
-  marginTop: "10px",
-};
+// const boxesStyle = {
+//   display: "flex",
+//   marginTop: "10px",
+// };
 
-function App(){
-  console.log("App 컴포넌트 리렌더링");
+// function App(){
+//   console.log("App 컴포넌트 리렌더링");
 
-  const [count, setCount] = useState(0);
+//   const [count, setCount] = useState(0);
 
-  const plusBtn = () => {
-    setCount(count + 1);
+//   const plusBtn = () => {
+//     setCount(count + 1);
+//   };
+
+//   const minusBtn = () => {
+//     setCount(count - 1);
+//   };
+
+//   const initCount = useCallback(() => {
+//     console.log(`[COUNT 변경] ${count}에서 0으로 변경!`);
+//     setCount(0);
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <h3>카운트 예제</h3>
+//       <p>현재 카운트 : {count}</p>
+//       <button onClick={plusBtn}>+</button>
+//       <button onClick={minusBtn}>-</button>
+//       <div style={boxesStyle}>
+//         <Box1 initCount={initCount} />
+//         <Box2 />
+//         <Box3 />
+//       </div>
+//     </div>
+//   );
+// }
+
+import HeavyComponent from "./components/HeavyComponent";
+
+function App() {
+  const navStyle = {
+    backgroundColor: "yellow",
+    marginBottom: "30px",
   };
-
-  const minusBtn = () => {
-    setCount(count - 1);
+  
+  const footerStyle = {
+    backgroundColor: "green",
+    marginTop: "30px",
   };
-
-  const initCount = useCallback(() => {
-    console.log(`[COUNT 변경] ${count}에서 0으로 변경!`);
-    setCount(0);
-  }, [count]);
-
+  
   return (
-    <div>
-      <h3>카운트 예제</h3>
-      <p>현재 카운트 : {count}</p>
-      <button onClick={plusBtn}>+</button>
-      <button onClick={minusBtn}>-</button>
-      <div style={boxesStyle}>
-        <Box1 initCount={initCount} />
-        <Box2 />
-        <Box3 />
-      </div>
+  	<div>
+      <nav style={navStyle}>네비게이션 바</nav>
+ 	  <HeavyComponent />
+      <footer style={footerStyle}>푸터</footer>
     </div>
   );
 }
