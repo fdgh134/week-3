d377994707ed47b0359557b44110ff619f1cfb1a
yoom <fdgh134@naver.com>
2024-12-11 23:00:23 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import store from "./redux/config/configStore";
-import { Provider } from 'react-redux';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
